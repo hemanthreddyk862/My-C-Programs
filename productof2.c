de3403567e2be17d7e8837668176b5473fc754ef
HEMANTHREDDY <hemanthreddykarri9@gmail.com>
2020-07-14 20:52:37 +0530

    versioning my c stuff

---
+++ productof2.c
@@ -0,0 +1,18 @@
+/* product of two number with run time allocation of variable values*/
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	float a,b,c;
+	printf("a value:");
+	scanf("%f",&a);
+	printf("b value:");
+	scanf("%f",&b);
+	
+	c=a*b;
+	
+	printf("product is :%f",c);
+}
+
